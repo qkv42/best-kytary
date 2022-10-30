d553abb64198d64b2b54c0569596b474d4690418
Klára Vytisková <klara.vytiskova@applifting.cz>
2022-10-30 13:37:51 +0100

    feat: typeorm integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { GuitarService } from './services/guitar.service';
 import { BassService } from './services/bass.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'kytara',
+      database: 'best-kytary',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [GuitarService, BassService],
 })
