9ce783321b8f6fcc22f25235945b0c76ca484cfb
Klára Vytisková <klara.vytiskova@applifting.cz>
2022-12-09 18:44:49 +0100

    feat: guitar crud endpoints

--- src/controllers/guitar.controller.ts
+++ src/controllers/guitar.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Guitars } from 'src/entities/guitar.entity';
+import { Guitar } from 'src/interface';
 import { GuitarService } from 'src/services/guitar.service';
 import { throwNotFoundError } from 'src/utils';
 
@@ -15,12 +25,33 @@ export class GuitarController {
     if (!guitar) {
       throwNotFoundError("Guitar with this ID doesn't exist.");
     }
-
     return guitar;
   }
 
   @Get('guitars')
   getGuitars(): Promise<Guitars[]> {
     return this.guitarService.getGuitars();
+  }
+
+  @Post('/guitar')
+  async createGuitar(@Body() params: Guitar): Promise<Guitars> {
+    const createdGuitar = await this.guitarService.createGuitar(params);
+    return createdGuitar;
+  }
+
+  @Delete('/guitar/:id')
+  @HttpCode(202)
+  async deleteExample(@Param('id') id: number) {
+    await this.guitarService.deleteGuitar(id);
+  }
+
+  @Patch('/guitar/:id')
+  @HttpCode(202)
+  updateExample(
+    @Param('id') id: number,
+    @Body() params: Guitar,
+  ): Promise<void> {
+    const updatedGuitar = this.guitarService.updateGuitar(id, params);
+    return updatedGuitar;
   }
 }
